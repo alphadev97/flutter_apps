d5e76388e401c01c2808cea3fe951ab93adeae54
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-04-19 22:35:20 +0500

    variables

---
+++ doc/b_variables.dart
@@ -0,0 +1,62 @@
+void main() {
+  // Variables
+
+  // <datatype> <variablename> = value;
+
+  // int firstvalue =  302;
+  // int secondvalue = 51;
+//   double firstvalue = 302.50;
+//   double secondvalue = 102.22;
+
+//   // First Value
+//   print(firstvalue);
+
+//   // Second Value
+//   print(secondvalue);
+
+//   // Third Value
+//   print(firstvalue * secondvalue);
+
+//   // Fourth Value
+//   print(firstvalue + secondvalue);
+
+//   // Fourth Value
+//   print(firstvalue - secondvalue);
+
+//   String firstvalue = "usama";
+
+//   print(firstvalue);
+
+//   bool ischild = true;
+
+//   print(ischild);
+
+//   dynamic somevalue = true; // is not recommanded
+
+//   print(somevalue);
+
+// ============================================
+
+// var/final/const variableName = value;
+
+//   var someValue1 = "10";
+//   final someValue2 = "10";
+//   const someValue3 = "10";
+
+//   print(someValue1);
+//   print(someValue2);
+//   print(someValue3);
+
+//   print("-------------------");
+
+//   someValue1 = "10001";
+
+// ===============================================
+
+// Optional Variables
+// String/int/bool and null
+
+//   String? someValue = "";
+
+//   int? some = null;
+}
