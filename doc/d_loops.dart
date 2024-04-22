57ff6b969c16044b286ef0eab1459160e754a030
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-04-22 19:44:22 +0500

    loops

--- doc/d_loops.dart
+++ doc/d_loops.dart
@@ -7,7 +7,36 @@ void main() {
 
 // }
 
-  for (int i = 1; i <= 10; i++) {
-    print("Hello World ${i}");
+  // for (int i = 0; i <= 10; i++) {
+  //   String hi = "Hello World";
+  //   print(hi.substring(0, 5));
+  //   print("Hello World ${i}");
+  // }
+
+  // for in loop
+
+  String value = "Hello";
+
+  for (int i = 0; i < value.length; i++) {
+    if (i == 1 || i == 2 || i == 3) {
+      continue;
+    }
+    print(value[i]);
   }
+
+  // while loop
+
+  // while (i < value.length) {
+  //   print(value[i]);
+  //   i++;
+  // }
+
+  // Do While Loops
+
+  // int i = 0;
+
+  // do {
+  //   print(value[i]);
+  //   i++;
+  // } while (i < value.length);
 }
