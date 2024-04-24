1e03cc12159084cfeb757141f969e359f4c00886
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-04-25 00:46:17 +0500

    oop

---
+++ doc/h_oop.dart
@@ -0,0 +1,47 @@
+void main() {
+  // Object Oriented Programming
+  // 1. Polymorphism
+  // Animal cat = Cat();
+  // cat.sound();
+  // cat = Dog();
+  // cat.sound();
+  // 2. Abstraction
+  // Animal anim = Cat();
+  // anim.sound();
+  // Animal anim2 = Dog();
+  // anim2.sound();
+  // 3. Inheritance
+  // Class extends
+  // 4. Encapsulation
+  // It hides the data from outside world
+}
+
+// class Person {
+//   String _name;
+// }
+
+// abstract class Animal {
+//   void sound();
+// }
+
+// 1. Polymorphism
+
+// class Animal {
+//   void sound() {
+//     print("Animal making sound!");
+//   }
+// }
+
+// class Cat extends Animal {
+//   @override
+//   void sound() {
+//     print("Cat making sound!");
+//   }
+// }
+
+// class Dog extends Animal {
+//   @override
+//   void sound() {
+//     print("Dog making sound!");
+//   }
+// }
