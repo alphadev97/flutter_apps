80e9b2cc5daf7148be1ec85ad6bd9976ce1f744e
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-04-25 00:53:59 +0500

    mixins

---
+++ doc/i_mixins.dart
@@ -0,0 +1,22 @@
+void main() {
+  // Mixin
+  // Mixes in
+  final anim = Animal();
+
+  anim.fn();
+}
+
+mixin Jump {
+  int jumping = 10;
+}
+
+mixin Scream {
+  bool isScreaming = false;
+}
+
+class Animal with Jump, Scream {
+  void fn() {
+    print(jumping);
+    print(isScreaming);
+  }
+}
