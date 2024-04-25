ddc3319e634f2315a2f9a0d5207d7bace634d93a
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-04-26 00:35:44 +0500

    Futures

---
+++ doc/o_futures.dart
@@ -0,0 +1,21 @@
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+
+// void main() async {
+//   var url = Uri.https('jsonplaceholder.typicode.com', 'users/1');
+
+//   try {
+//     final res = await http.get(url);
+//     print(jsonDecode(res.body)["name"]);
+//   } catch (e) {
+//     print(e);
+//   }
+
+//   print("Final Print");
+// }
+
+// Future<String> giveAResultAfter2Sec() async {
+//   return Future.delayed(Duration(seconds: 2), () {
+//     return "Hey!!!!!!";
+//   });
+// }
